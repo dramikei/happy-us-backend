0e0d714fdf7faa00cf6bc5bcff22e4c55092a669
sarcastic-verma <shivamthegreat.sv@gmail.com>
2021-05-31 13:48:09 +0530

    :goal_net: updated exceptions.filter and logging

--- src/utils/exceptions.filter.ts
+++ src/utils/exceptions.filter.ts
@@ -7,6 +7,7 @@ import {
 } from '@nestjs/common';
 import { logger } from './loggerInstance';
 import { BaseResponse } from './response.interceptor';
+import { ServerResponse } from 'http';
 
 @Catch()
 export class ExceptionsFilter implements ExceptionFilter {
@@ -36,11 +37,24 @@ export class ExceptionsFilter implements ExceptionFilter {
     logger.error(
       `-------------End Error in path ${request.url}-----------------`,
     );
-    return response.status(exceptionDetails.code).send({
+
+    const errorResponse: BaseResponse<null> = {
       message: exceptionDetails.message,
       status: exceptionDetails.status,
       path: request.url,
       data: null,
-    } as BaseResponse<null>);
+    };
+
+    // fastify uses middlewares differently
+    // It uses native response instead of Reply wrapper
+    if (response instanceof ServerResponse) {
+      response.writeHead(exceptionDetails.code, {
+        'content-type': 'application/json',
+      });
+      response.write(JSON.stringify(errorResponse));
+      response.end();
+    } else {
+      return response.code(exceptionDetails.code).send(errorResponse);
+    }
   }
 }
--- src/utils/loggerInstance.ts
+++ src/utils/loggerInstance.ts
@@ -1,3 +1,13 @@
 import { Logger } from '@nestjs/common';
 
 export const logger = new Logger();
+
+export const ReqLogger = (req: Request, code: number, message: string) => {
+  const method = code >= 400 ? 'error' : 'info';
+  logger[method]({
+    message,
+    method: req.method,
+    headers: req.headers,
+    ip: req.url,
+  });
+};
