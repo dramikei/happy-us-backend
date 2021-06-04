53e8426fd0938bb3924e9b1d2192cfc09978d4f1
sarcastic-verma <shivamthegreat.sv@gmail.com>
2021-06-05 01:43:50 +0530

    :ambulance: Fixed major issue with minor code change ;-)

--- src/utils/response.interceptor.ts
+++ src/utils/response.interceptor.ts
@@ -22,9 +22,14 @@ export class ResponseInterceptor<T>
     context: ExecutionContext,
     next: CallHandler,
   ): Observable<BaseResponse<T>> {
+    const request = context.switchToHttp().getRequest();
     return next.handle().pipe(
       map((data) => ({
         data,
+        tokens: {
+          accessToken: request.raw?.info?.accessToken,
+          refreshToken: request.raw?.info?.refreshToken,
+        },
         message: data?.message || 'No error occurred',
         path: context.switchToHttp().getRequest().url,
         status: 'Success', // if a request reaches this interceptor it's execution is successful
