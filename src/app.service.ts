52c714b733bc68012888e28b80e480312c6a9c2f
sarcastic-verma <shivamthegreat.sv@gmail.com>
2021-06-27 18:49:12 +0530

    :children_crossing: Added heroku sleep hack

--- src/app.service.ts
+++ src/app.service.ts
@@ -1,8 +1,15 @@
 import { Injectable } from '@nestjs/common';
+import axios from 'axios';
 
 @Injectable()
 export class AppService {
   ping(ip: string): string {
+    // heroku sleep hack
+    setTimeout(async () => {
+      await axios.get('https://happyusapi.herokuapp.com/api/ping');
+      console.log('Keeping the server alive!!');
+    }, 20 * 60 * 1000); // 10 minutes interval
+
     return `Server Pinged from ${ip}`;
   }
 }
